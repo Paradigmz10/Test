93d805ada1de93fa558ecf549afee61c9f557193
Zach Holland <80822568+zhollandio@users.noreply.github.com>
2023-09-24 12:40:35 -0400

    Update country_scaper.py

--- country_scaper.py
+++ country_scaper.py
@@ -1,5 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
+import pickle
+import os
+import logging
+from datetime import datetime, timedelta
+
+logging.basicConfig(level=logging.INFO)
+
+CACHE_FILE = 'cache.pkl'
+CACHE_EXPIRATION = timedelta(days=1)
 
 def fetch_continent_mapping():
     url = "https://en.wikipedia.org/wiki/List_of_sovereign_states_and_dependent_territories_by_continent_(data_file)"
@@ -30,13 +39,35 @@ def fetch_country_codes(continent_mapping):
         countries.append((name, iso_alpha_2, iso_alpha_3, iso_numeric, continent))
     return countries
 
+def cache_data(data):
+    with open(CACHE_FILE, 'wb') as f:
+        pickle.dump((datetime.now(), data), f)
+
+def load_cached_data():
+    if not os.path.exists(CACHE_FILE):
+        return None
+    with open(CACHE_FILE, 'rb') as f:
+        timestamp, data = pickle.load(f)
+        if datetime.now() - timestamp < CACHE_EXPIRATION:
+            return data
+    return None
+
 def main():
-    continent_mapping = fetch_continent_mapping()
-    countries = fetch_country_codes(continent_mapping)
-    print("countries = (")
-    for country in countries:
-        print(f'("{country[0]}", "{country[1]}", "{country[2]}", "{country[3]}", "{country[4]}"),')
-    print(")")
+    countries = load_cached_data()
+    if not countries:
+        logging.info("Fetching data from Wikipedia...")
+        continent_mapping = fetch_continent_mapping()
+        countries = fetch_country_codes(continent_mapping)
+        cache_data(countries)
+    else:
+        logging.info("Loading data from cache...")
+    
+    continent_filter = input("Enter continent name to filter (leave empty for all): ").capitalize()
+    filtered_countries = [country for country in countries if continent_filter in country[4]]
+    
+    print("Countries:")
+    for country in filtered_countries:
+        print(f'("{country[0]}", "{country[1]}", "{country[2]}", "{country[3]}", "{country[4]}")')
 
 if __name__ == "__main__":
     main()
